e19fbb7828595e72580d10f0753fa4b598406c34
Yvan Burrie <yvan.burrie@hotmail.com>
2018-08-31 23:36:18 +1000

    Redo thread test
    The entire thread previously create was inadequate due to its innacurate
    timing, for example, slightly random number of threads occuring at
    inaccurate time in total. The thread is now very accurate: it loops the
    exact number of times specified and (depending on the machine used)
    finishes in the total time expected by the microsecond.
    
    Also, the entire thread is more simple due to removing certain
    variables.

--- src/Test.java
+++ src/Test.java
@@ -34,30 +34,20 @@ public class Test {
 class TestThread extends Thread {
 
     /**
+     * Defines the number of nanoseconds in each second.
+     */
+    final public static double NANOSECONDS_PER_SECOND = 1000000000;
+
+    /**
      * Specifies the number of seconds for each second of the thread.
      * For example, set to 60 so that 1 minute is accelerated to 1 second.
      */
     protected double speed = 0.5;
 
     /**
-     *
+     * Specifies the expiration of the thread limited in number of seconds.
      */
-    protected double speedRatio = 1 / this.speed;
-
-    /**
-     *
-     */
-    protected double micsPerSec = this.speedRatio * 1000;
-
-    /**
-     *
-     */
-    protected double nansPerSec = this.micsPerSec * 1000;
-
-    /**
-     *
-     */
-    protected double nansPerSecRemainder = this.nansPerSec % this.micsPerSec;
+    protected double limit = this.speed * 1;
 
     /**
      *
@@ -65,11 +55,7 @@ class TestThread extends Thread {
     @Override
     public synchronized void start() {
 
-        System.out.println("Attempting to start thread with the following time attributes:");
-        System.out.println(" - Speed: " + this.speed);
-        System.out.println(" - Speed Ratio: " + this.speedRatio);
-        System.out.println(" - Microseconds or Nanoseconds per second: " + this.micsPerSec + ", " + this.nansPerSec);
-        System.out.println(" - Nanoseconds remainder from microseconds: " + this.nansPerSecRemainder);
+        System.out.println("Attempting to start thread at speed: " + this.speed);
 
         super.start();
     }
@@ -79,15 +65,31 @@ class TestThread extends Thread {
      */
     public void run() {
 
+        long firstTime = System.nanoTime();
+        long startTime = firstTime;
+
+        long loopsAttempted = 0;
+        long loopsEvaluated = 0;
+
         for (; ; ) {
 
-            System.out.println("Current time in milliseconds and nanoseconds (respectively): " + System.currentTimeMillis() + ", " + System.nanoTime());
+            loopsAttempted++;
 
-            try {
-                Thread.sleep((long) this.micsPerSec, (int) this.nansPerSecRemainder);
-            } catch (InterruptedException exception) {
-                return;
+            long pauseTime = System.nanoTime() - startTime;
+
+            /* Note that at this point, we may wish to change the speed of the thread while running. */
+
+            if (pauseTime >= NANOSECONDS_PER_SECOND / this.speed) {
+                startTime += NANOSECONDS_PER_SECOND / this.speed;
+
+                /* Any code needed for threaded-execution should be placed here. */
+
+                if (++loopsEvaluated >= this.limit) break;
             }
         }
+
+        System.out.println("Total thread loops attempted: " + loopsAttempted);
+        System.out.println("Total thread loops evaluated: " + loopsEvaluated);
+        System.out.println("Total thread time in seconds: " + ((System.nanoTime() - firstTime) / NANOSECONDS_PER_SECOND));
     }
 }
