373f60fb8bb9ea87b59b9e428dde184d0b2d128f
Yvan Burrie <yvan.burrie@hotmail.com>
2018-09-07 07:45:45 +1000

    Update trigger system
    Make trigger sync events and actions.
    Also add more explanations.

--- src/Trigger.java
+++ src/Trigger.java
@@ -5,20 +5,26 @@ import org.json.simple.JSONObject;
 import java.util.ArrayList;
 
 /**
- *
+ * Each trigger acts like a logical-gateway where events are tried on a thread and fire actions if successful.
  */
-public class Trigger implements JsonDeserializable {
+public class Trigger implements JsonDeserializable, JsonSerializable, Synchronizable {
 
     private Automator automator;
 
     public Automator getAutomator() { return automator; }
 
+    /**
+     * The user should provide a name to each trigger to identify them.
+     */
     private String name = "Un-named Trigger";
 
     public String getName() { return name; }
 
     public void setName(String name) { this.name = name; }
 
+    /**
+     * The user should add a description to the trigger to remember its behavior.
+     */
     private String description = "";
 
     public String getDescription() { return description; }
@@ -28,30 +34,47 @@ public class Trigger implements JsonDeserializable {
     public enum Status {
         ASLEEP,
         READY,
+        RUNNING,
+        TRIED,
         DONE,
     }
 
+    /**
+     * When each trigger is tried by the thread, it should have a change in status.
+     */
     private Status status = Status.ASLEEP;
 
     public Status getStatus() { return status; }
 
     public void setStatus(Status status) { this.status = status; }
 
+    /**
+     * When a trigger is looping, this means it will not be disabled after it is successful and the thread will try it again.
+     */
     private boolean looping = false;
 
     public boolean isLooping() { return looping; }
 
     public void setLooping(boolean looping) { this.looping = looping; }
 
-    abstract protected class Event implements JsonDeserializable {
+    /**
+     * Events in each trigger are tried for checking whether the trigger can fire actions.
+     */
+    abstract protected class Event implements JsonDeserializable, JsonSerializable, Synchronizable {
 
         protected Trigger trigger;
 
-        final static String jsonMacro = "DEFAULT";
+        final static String TYPE = "DEFAULT";
 
+        /**
+         * Determines whether the event was successful upon trying.
+         */
         protected boolean successful = false;
 
-        public boolean isSuccessful() { return successful; }
+        /**
+         * All events within a trigger can be AND'ED or OR'ED.
+         */
+        protected boolean orWithPrevious = false;
 
         public Event(Trigger trigger) {
 
@@ -69,15 +92,42 @@ public class Trigger implements JsonDeserializable {
         }
     }
 
+    /*
+    protected class ApparatusDetectionEvent extends Event {
+
+        final public static String JSON_MACRO = "APPARATUS_DETECTION";
+
+        private Apparatus apparatus;
+
+        private boolean detected = false;
+
+        public boolean isDetected() { return this.detected; }
+
+        public void setDetected(boolean detected) { this.detected = detected; }
+
+        public void ApparatusDetectionEvent(Trigger trigger, Apparatus apparatus) {
+
+            //super::Event(trigger);
+
+            this.apparatus = apparatus;
+        }
+
+        public void ApparatusDetectionEvent(Trigger trigger, long apparatusId) {
+
+            Apparatus apparatus = this.trigger.getAutomator().getApparatus(apparatusId);
+
+            this.ApparatusDetectionEvent(trigger, apparatus);
+        }
+    }
+    */
+
+    private ArrayList<Event> events = new ArrayList<>();
+
+    abstract protected class Action implements Synchronizable {
+
     }
 
-    private ArrayList<Event> events;
-
-    class Action {
-
-    }
-
-    private ArrayList<Action> actions;
+    private ArrayList<Action> actions = new ArrayList<>();
 
     public Trigger(Automator automator) {
 
@@ -98,16 +148,31 @@ public class Trigger implements JsonDeserializable {
 
         if (this.status != Status.READY) return;
 
-        //for (this.events: Event event) {
+        this.status = Status.RUNNING;
 
-        //event.sync();
+        boolean eventsResult = false;
 
-        //if (!event.isSuccessful()) return;
-        //}
+        /* Try out each event: */
+        for (int i = 0; i < this.events.size(); i++) {
+            Event event = this.events.get(i);
+            event.synchronize(loopsPerSecond);
+            if (event.orWithPrevious) {
+                eventsResult |= event.successful;
+            } else {
+                eventsResult &= event.successful;
+            }
+            if (!eventsResult) {
+                this.status = Status.TRIED;
+                return;
+            }
+        }
 
-        //for (this.actions: Action actiom) {
+        /* Fire all actions: */
+        for (int i = 0; i < this.actions.size(); i++) {
+            Action action = this.actions.get(i);
+            action.synchronize(loopsPerSecond);
+        }
 
-        //actiom.run();
-        //}
+        this.status = Status.DONE;
     }
 }
