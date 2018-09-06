cd453772be479673e56099f174746cd346b3bd2b
Yvan <yvan.burrie@my.jcu.edu.au>
2018-09-06 17:20:45 +1000

    Add scenario class

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,10 +1,52 @@
-/**
+/*
  * @author Yvan Burrie
  */
 
+import org.json.simple.JSONObject;
+
 /**
- *
+ * The simulation system hijacks the automation system to alter its state of reality.
  */
-public class Simulator extends Automator {
-    
+final public class Simulator extends Automator {
+
+    /**
+     *
+     */
+    protected Scenario[] scenarios;
+
+    /**
+     *
+     */
+    public int numScenarios() {
+        return this.scenarios.length;
+    }
+
+    public Scenario getScenario(int scenarioId) {
+        return this.scenarios[scenarioId];
+    }
+
+    public void setScenario(int scenarioId, Scenario scenario) {
+        this.scenarios[scenarioId] = scenario;
+    }
+
+    public Simulator() {
+
+    }
+
+    @Override
+    public void jsonDeserialize(JSONObject jsonObject) {
+
+        super.jsonDeserialize(jsonObject);
+
+        Object bufferObject;
+
+
+    }
+}
+
+/**
+ * A scenario consists of customizable consequences injected into the automation system.
+ */
+abstract class Scenario {
+
 }
