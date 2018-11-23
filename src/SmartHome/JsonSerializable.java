81e2a020c7cb4aa4a5b15f1ef77d1528be14214c
Yvan Burrie <jc444304@ad.jcu.edu.au>
2018-11-23 14:51:54 +1000

    Update JsonSerializable.java
    Also add exception classes to be used for the JSON reading and writing.

--- src/SmartHome/JsonSerializable.java
+++ src/SmartHome/JsonSerializable.java
@@ -2,7 +2,7 @@
 
 package SmartHome;
 
-import org.json.simple.JSONObject;
+import org.json.simple.*;
 
 /**
  * Use JSON for the configurations instead of re-inventing the wheel.
@@ -25,10 +25,45 @@ interface JsonSerializable extends JsonSerialization {
 /**
  *
  */
-interface JsonDeserializable extends JsonSerialization {
+interface JsonDeserializable extends JsonSerializable {
 
     /**
      *
      */
     void jsonDeserialize(JSONObject jsonObject);
+}
+
+/**
+ * An error caused when using the above JSON interfaces.
+ */
+abstract class JsonSerializationError extends RuntimeException {
+
+    protected JsonSerialization jsonObject;
+
+    protected String erorrMessage;
+
+    JsonSerializationError (JsonSerializable object, String message) {
+        this.jsonObject = object;
+        this.erorrMessage = message;
+    }
+}
+
+/**
+ * An error caused when writing JSON data.
+ */
+class JsonSerializeError extends JsonSerializationError {
+
+    JsonSerializeError(JsonSerializable object, String message) {
+        super(object, message);
+    }
+}
+
+/**
+ * An error caused when reading JSON data.
+ */
+class JsonDeserializeError extends JsonSerializationError {
+
+    JsonDeserializeError(JsonSerializable object, String message) {
+        super(object, message);
+    }
 }
