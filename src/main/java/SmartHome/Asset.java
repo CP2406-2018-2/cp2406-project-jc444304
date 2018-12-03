318774e4ee56d33ca568a3d8b8cd09fc7db078ab
Yvan Burrie <yvan.burrie@hotmail.com>
2018-12-03 22:21:52 +1000

    Add dimension points to Asset class
    Allows X-Y points to be drawn for vectors.

--- src/main/java/SmartHome/Asset.java
+++ src/main/java/SmartHome/Asset.java
@@ -36,6 +36,8 @@ abstract class Entity extends Asset {
 
     String description;
 
+    ArrayList<Point> dimension;
+
     public Entity(Automator automator) {
         super(automator);
     }
@@ -61,6 +63,25 @@ abstract class Entity extends Asset {
         if (objectBuffer instanceof String) {
             description = (String) objectBuffer;
         }
+        objectBuffer = entityBuffer.get("Dimension");
+        if (objectBuffer instanceof JSONArray) {
+            JSONArray dimensionBuffer = (JSONArray) objectBuffer;
+            for (Object elementBuffer : dimensionBuffer) {
+                if (elementBuffer instanceof JSONArray) {
+                    JSONArray pointBuffer = (JSONArray) elementBuffer;
+                    int pointX = 0, pointY = 0;
+                    objectBuffer = pointBuffer.get(0);
+                    if (objectBuffer instanceof Integer) {
+                        pointX = (int) objectBuffer;
+                    }
+                    objectBuffer = pointBuffer.get(1);
+                    if (objectBuffer instanceof Integer) {
+                        pointY = (int) objectBuffer;
+                    }
+                    dimension.add(new Point(pointX, pointY));
+                }
+            }
+        }
     }
 
     @Override
@@ -77,6 +98,26 @@ abstract class Entity extends Asset {
         if (description != null) {
             entityBuffer.put("Description", description);
         }
+        if (dimension != null) {
+            JSONArray dimensionBuffer = new JSONArray();
+            for (Point point : dimension) {
+                JSONArray pointBuffer = new JSONArray();
+                pointBuffer.add(point.x);
+                pointBuffer.add(point.y);
+            }
+            entityBuffer.put("Dimension", dimensionBuffer);
+        }
+
         return entityBuffer;
+    }
+
+    class Point {
+
+        int x, y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
