a5bc6945ea0762905ae97742ca2916c55f68703f
Yvan Burrie <yvan.burrie@hotmail.com>
2018-12-08 16:44:48 +1000

    Update Guide

--- src/main/java/GuideFrame.java
+++ src/main/java/GuideFrame.java
@@ -8,10 +8,13 @@ class GuideFrame extends WebFrame {
 
         super(MainFrame.APPLICATION_NAME + " - Guide", 600, 500);
         Platform.runLater(() -> webView.getEngine().loadContent(
-            contentHeader +
-            "<h1>Guide to using Auto Home</h1>" +
-            "<p>Welcome</p>" +
-            contentFooter
+                contentHeader +
+                        "<h1>Guide to using " + MainFrame.APPLICATION_NAME + "</h1>" +
+                        "<p>Welcome</p>" +
+                        "<h2>Triggers</h2>" +
+                        "<p>Triggers is essential for <i>" + MainFrame.APPLICATION_NAME + "</i> automated ability." +
+                        "It allows you to create a set of triggers that contain events followed by actions.</p>" +
+                        contentFooter
         ));
     }
 }
