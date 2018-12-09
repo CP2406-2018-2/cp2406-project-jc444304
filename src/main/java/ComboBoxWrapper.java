e8d6c2343e4814ef4bcf4597d7f733eb75f85470
Yvan Burrie <yvan.burrie@hotmail.com>
2018-12-09 10:20:21 +1000

    Create Combox-Box Warapper

---
+++ src/main/java/ComboBoxWrapper.java
@@ -0,0 +1,32 @@
+// Author: Yvan Burrie
+
+import javax.swing.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class ComboBoxWrapper<K> extends JComboBox<String> {
+
+    private HashMap<K, String> buffers = new HashMap<>();
+
+    ComboBoxWrapper() {
+
+    }
+
+    void addItem(String item, K key) {
+
+        super.addItem(item);
+        buffers.put(key, item);
+    }
+
+    K getSelectedKey() {
+
+        for (HashMap.Entry<K, String> buffer : buffers.entrySet()) {
+            Object selectedItem = getSelectedItem();
+            if (selectedItem instanceof String && selectedItem.equals(buffer.getValue())) {
+                return buffer.getKey();
+            }
+        }
+        return null;
+    }
+}
